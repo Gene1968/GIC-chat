9e73418810344e664ee91ebe98e11337be72732d
Gene Biondo <gene@unforgettable.com>
2019-09-17 17:27:02 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    gitRepo: 'https://github.com/Gene1968/GIT-repo.git',
+    gitRepo: 'https://github.com/Gene1968/GIC-repo.git',
     messageCheckInterval: 500,
     branchesCheckInterval: 5000
 };
